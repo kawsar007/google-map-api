ebd754233df3d060b5be1cf20d9d65e5658669b4
kawsar007 <imkawsar007@gmail.com>
2024-03-16 14:16:50 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Map from "./google-map/map";
+
+function App() {
+  return (
+    <>
+      <Map />
+    </>
+  );
+}
+
+export default App;
---
+++ src/google-map/map.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    <div className="container">
+      <div className="controls">
+        <h2>Left Sidebar</h2>
+      </div>
+      <div className="map">This is map Section</div>
+    </div>
+  );
+};
+
+export default Map;
