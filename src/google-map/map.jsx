65d84e04e76881bb77d98aceaf0a7d5504e9f165
kawsar007 <imkawsar007@gmail.com>
2024-03-16 21:46:07 +0600

    Added place auto complete

--- src/google-map/map.jsx
+++ src/google-map/map.jsx
@@ -1,8 +1,10 @@
-import { GoogleMap } from "@react-google-maps/api";
-import React, { useCallback, useMemo, useRef } from "react";
+import { Circle, GoogleMap, Marker } from "@react-google-maps/api";
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import Places from "./places";
 
 const Map = () => {
   const mapRef = useRef();
+  const [office, setOffice] = useState();
   const center = useMemo(() => ({ lat: 23.3453453, lng: 90.543433 }), []);
   const options = useMemo(
     () => ({
@@ -20,6 +22,12 @@ const Map = () => {
     <div className="container">
       <div className="controls">
         <h2>Left Sidebar</h2>
+        <Places
+          setOffice={(position) => {
+            setOffice(position);
+            mapRef.current?.panTo(position);
+          }}
+        />
       </div>
       <div className="map">
         <GoogleMap
@@ -28,10 +36,50 @@ const Map = () => {
           options={options}
           onload={onload}
           mapContainerClassName="map-container"
-        ></GoogleMap>
+        >
+          {office && (
+            <>
+              <Marker position={office} icon="" />
+
+              <Circle center={office} radius={15000} options={closeOptions} />
+              <Circle center={office} radius={30000} options={middleOptions} />
+              <Circle center={office} radius={45000} options={farOptions} />
+            </>
+          )}
+        </GoogleMap>
       </div>
     </div>
   );
+};
+
+const defaultOptions = {
+  strokeOpacity: 0.5,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+};
+const closeOptions = {
+  ...defaultOptions,
+  zIndex: 3,
+  fillOpacity: 0.05,
+  strokeColor: "#8BC34A",
+  fillColor: "#8BC34A",
+};
+const middleOptions = {
+  ...defaultOptions,
+  zIndex: 2,
+  fillOpacity: 0.05,
+  strokeColor: "#FBC02D",
+  fillColor: "#FBC02D",
+};
+const farOptions = {
+  ...defaultOptions,
+  zIndex: 1,
+  fillOpacity: 0.05,
+  strokeColor: "#FF5252",
+  fillColor: "#FF5252",
 };
 
 export default Map;
--- src/google-map/places.jsx
+++ src/google-map/places.jsx
@@ -1,7 +1,56 @@
+/* eslint-disable react/prop-types */
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxList,
+  ComboboxOption,
+  ComboboxPopover,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 import React from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 
-const Places = () => {
-  return <div>Places</div>;
+const Places = ({ setOffice }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (val) => {
+    setValue(val, false);
+    clearSuggestions();
+    const results = await getGeocode({ address: val });
+    const { lat, lng } = await getLatLng(results[0]);
+    setOffice({ lat, lng });
+  };
+
+  console.log("My Info-->", { status, data });
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        className="combobox-input"
+        placeholder="Search office addredd..."
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
 };
 
 export default Places;
