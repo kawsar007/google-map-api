67adc61a10ae415a7ce3e64afa39e3e9615dc737
kawsar007 <imkawsar007@gmail.com>
2024-03-16 15:14:39 +0600

    Render map

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useLoadScript } from "@react-google-maps/api";
 import "./App.css";
 import Map from "./google-map/map";
 
 function App() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyDpb3E0DazmzFWmuybM77oi-Lm_C9Jal2k",
+    // googleMapsApiKey: "AIzaSyAM-Q69B72A3OjmUbRa1b1Zkf3lsed_BPU",
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
     <>
       <Map />
--- src/google-map/map.jsx
+++ src/google-map/map.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import { GoogleMap } from "@react-google-maps/api";
+import React, { useCallback, useMemo, useRef } from "react";
 
 const Map = () => {
+  const mapRef = useRef();
+  const center = useMemo(() => ({ lat: 23.3453453, lng: 90.543433 }), []);
+  const options = useMemo(
+    () => ({
+      mapId: "3d9b72b90526e8f1",
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
+  const onload = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
   return (
     <div className="container">
       <div className="controls">
         <h2>Left Sidebar</h2>
       </div>
-      <div className="map">This is map Section</div>
+      <div className="map">
+        <GoogleMap
+          zoom={10}
+          center={center}
+          options={options}
+          onload={onload}
+          mapContainerClassName="map-container"
+        ></GoogleMap>
+      </div>
     </div>
   );
 };
